22c790ae2fe085be3c8c27e3cf69ef4064f3bf45
JavierNavarro12 <navarrojavi107@gmail.com>
2025-06-22 19:45:32 +0200

    fix: Corregir script de generación de sitemap asíncrono

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -18,26 +18,30 @@ const pages = [
 
 const domain = 'https://endlessgoalsnutrition.com';
 
-const sitemap = `
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${pages
-      .map((page) => {
-        const route = page ? `/${page}` : '';
-        return `
-          <url>
-            <loc>${domain}${route}</loc>
-            <lastmod>${new Date().toISOString()}</lastmod>
-            <changefreq>monthly</changefreq>
-            <priority>${page ? '0.8' : '1.0'}</priority>
-          </url>
-        `;
-      })
-      .join('')}
-  </urlset>
-`;
+const generateSitemap = async () => {
+  const sitemap = `
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${pages
+        .map((page) => {
+          const route = page ? `/${page}` : '';
+          return `
+            <url>
+              <loc>${domain}${route}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>monthly</changefreq>
+              <priority>${page ? '0.8' : '1.0'}</priority>
+            </url>
+          `;
+        })
+        .join('')}
+    </urlset>
+  `;
 
-const formattedSitemap = prettier.format(sitemap, { parser: 'html' });
+  const formattedSitemap = await prettier.format(sitemap, { parser: 'html' });
 
-fs.writeFileSync(path.resolve(__dirname, '../public/sitemap.xml'), formattedSitemap);
+  fs.writeFileSync(path.resolve(__dirname, '../public/sitemap.xml'), formattedSitemap);
 
-console.log('Sitemap generated successfully!');
+  console.log('Sitemap generated successfully!');
+};
+
+generateSitemap();
