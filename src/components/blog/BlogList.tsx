2839ec38512d990ba98e0528f3297c2754a43847
JavierNavarro12 <navarrojavi107@gmail.com>
2025-07-09 20:44:59 +0200

    chore: Elimina imports no usados de auth y firebase/auth en BlogList.tsx

--- src/components/blog/BlogList.tsx
+++ src/components/blog/BlogList.tsx
@@ -4,8 +4,6 @@ import { Link } from 'react-router-dom';
 import { ContentfulService } from '../../services/contentful';
 import Loader from '../shared/Loader';
 import { likesService } from '../../services/likesService';
-import { auth } from '../../firebase';
-import { onAuthStateChanged, User } from 'firebase/auth';
 
 const BlogList: React.FC = () => {
   const [blogs, setBlogs] = useState<any[]>([]);
